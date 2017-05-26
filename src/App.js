5fc537981a454e2c7b51e0724b241825a72c26cb
Mariano Heller <marianoheller91@gmail.com>
2017-05-26 10:59:32 -0300

    init

--- src/App.js
+++ src/App.js
@@ -6,16 +6,80 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="pure-g">
+          <div className="pure-u-0 pure-u-sm-3 pure-u-md-6"></div>
+          <div className="pure-u-1 pure-u-sm-18 pure-u-md-12">
+            <Title></Title>
+            <Clock></Clock>
+            <ButtonArea></ButtonArea>
+            <ConfigDialog></ConfigDialog>
+          </div>
+          <div className="pure-u-0 pure-u-sm-3 pure-u-md-6"></div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
 }
 
+
+
+class Title extends Component {
+  render() {
+    return (
+      <h1 id="title">Pomodori per Tutti</h1>
+    )
+  }
+}
+
+
+
+class Clock extends Component {
+  render() {
+    return (
+      <div className="pure-g">
+        <div className="pure-u-1">
+          <div id="clock-container">
+            <p>Clock!</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+class ButtonArea extends Component {
+  render() {
+    return (
+      <div className="pure-g">
+        <div className="pure-u-1">
+          <ul>
+            <li><button href="#popup1">A</button></li>
+            <li><button>B</button></li>
+            <li><button>C</button></li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+class ConfigDialog extends Component {
+  render() {
+    return (
+      <div id="popup1" class="overlay">
+        <div class="popup">
+          <h2>Here i am</h2>
+          <a class="close" href="#">&times;</a>
+          <div class="content">
+            Thank to pop me out of that button, but now i'm done so you can close this window.
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
 export default App;
+
